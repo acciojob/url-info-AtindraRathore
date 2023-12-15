3ac24033a457d5eacb1d989c58d26065e04aa173
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-12-15 13:59:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 //your JS code here. If required.
-let n = JSON.stringify(https://webeditor.acciojob.com/?question=8be9d9d2-…un.app&platform=unrestricted&type=course&contest=);
+let n = JSON.stringify(window.location);
  alert('The length of the URL is: '+n.length);
